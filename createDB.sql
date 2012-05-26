a56422c05d2d91c4ba9dba7b25d88ca781c01fac
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2012-05-26 19:32:18 -0300

    actualizo db

--- createDB.sql
+++ createDB.sql
@@ -36,6 +36,7 @@ CREATE TABLE `ley` (
   `cant_votos_si` int(10) unsigned NOT NULL default '0',
   `cant_votos_no` int(10) unsigned NOT NULL default '0',
   `prioridad` int(10) unsigned NOT NULL,
+  `activa` tinyint(1) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
@@ -44,10 +45,10 @@ CREATE TABLE `ley` (
 --
 
 /*!40000 ALTER TABLE `ley` DISABLE KEYS */;
-INSERT INTO `ley` (`id`,`expediente`,`titulo_lleca`,`titulo_real`,`url_votamostodos`,`url_diputados`,`cant_votos_si`,`cant_votos_no`,`prioridad`) VALUES 
- (1,'2251-D-2012','Transferencia de los subtes a la ciudad','TRANSFERENCIA DE LOS SERVICIOS DE TRANSPORTE DE PASAJEROS A LA CIUDAD AUTONOMA DE BUENOS AIRES. RATIFICACION POR PARTE DEL H. CONGRESO DE LA NACION DE LOS CONVENIOS QUE SE CELEBREN ENTRE EL PODER EJECUTIVO NACIONAL Y LA CIUDAD AUTONOMA DE BUENOS AIRES.','transferencia-de-los-subtes-a-la-ciudad','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=2251-D-2012',0,0,1),
- (2,'5246-D-2011','Muerte Digna','DERECHO DEL PACIENTE, HISTORIA CLINICA Y CONSENTIMIENTO INFORMADO - LEY 26529 -. MODIFICACIONES, SOBRE MUERTE DIGNA Y CUIDADOS PALIATIVOS INTEGRALES.','muerte-digna','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=5246-D-2011',0,0,2),
- (3,'6259-D-2011','Nacionalizacion de YPF','NACIONALIZACION DE HIDROCARBUROS; EXPROPIACION DE ACTIVOS Y ACCIONES DE REPSOL YPF; CREACION DE YACIMIENTOS PETROLIFEROS DEL PUEBLO; DEROGACION DE LA LEY 24145.','nacionalizacion-de-ypf','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=6259-D-2011',0,0,3);
+INSERT INTO `ley` (`id`,`expediente`,`titulo_lleca`,`titulo_real`,`url_votamostodos`,`url_diputados`,`cant_votos_si`,`cant_votos_no`,`prioridad`,`activa`) VALUES 
+ (1,'2251-D-2012','Transferencia de los subtes a la ciudad','TRANSFERENCIA DE LOS SERVICIOS DE TRANSPORTE DE PASAJEROS A LA CIUDAD AUTONOMA DE BUENOS AIRES. RATIFICACION POR PARTE DEL H. CONGRESO DE LA NACION DE LOS CONVENIOS QUE SE CELEBREN ENTRE EL PODER EJECUTIVO NACIONAL Y LA CIUDAD AUTONOMA DE BUENOS AIRES.','transferencia-de-los-subtes-a-la-ciudad','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=2251-D-2012',4,2,1,1),
+ (2,'5246-D-2011','Muerte Digna','DERECHO DEL PACIENTE, HISTORIA CLINICA Y CONSENTIMIENTO INFORMADO - LEY 26529 -. MODIFICACIONES, SOBRE MUERTE DIGNA Y CUIDADOS PALIATIVOS INTEGRALES.','muerte-digna','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=5246-D-2011',0,0,2,1),
+ (3,'6259-D-2011','Nacionalizacion de YPF','NACIONALIZACION DE HIDROCARBUROS; EXPROPIACION DE ACTIVOS Y ACCIONES DE REPSOL YPF; CREACION DE YACIMIENTOS PETROLIFEROS DEL PUEBLO; DEROGACION DE LA LEY 24145.','nacionalizacion-de-ypf','http://www1.hcdn.gov.ar/proyxml/expediente.asp?fundamentos=si&numexp=6259-D-2011',0,0,3,1);
 /*!40000 ALTER TABLE `ley` ENABLE KEYS */;
 
 
@@ -58,37 +59,41 @@ INSERT INTO `ley` (`id`,`expediente`,`titulo_lleca`,`titulo_real`,`url_votamosto
 DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `id_facebook` varchar(45) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
+  `id_facebook` int(10) unsigned NOT NULL default '0',
+  `nombre` varchar(100) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usuario`
 --
 
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`id`,`id_facebook`,`nombre`) VALUES 
+ (4,522348324,NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 
 --
--- Definition of table `votos`
+-- Definition of table `voto`
 --
 
-DROP TABLE IF EXISTS `votos`;
-CREATE TABLE `votos` (
+DROP TABLE IF EXISTS `voto`;
+CREATE TABLE `voto` (
   `id_ley` int(10) unsigned NOT NULL,
   `id_usuario` int(10) unsigned NOT NULL,
   `voto` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id_ley`,`id_usuario`)
+  `momento` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `votos`
+-- Dumping data for table `voto`
 --
 
-/*!40000 ALTER TABLE `votos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `votos` ENABLE KEYS */;
+/*!40000 ALTER TABLE `voto` DISABLE KEYS */;
+INSERT INTO `voto` (`id_ley`,`id_usuario`,`voto`,`momento`) VALUES 
+ (1,4,1,'2012-05-26 18:39:07');
+/*!40000 ALTER TABLE `voto` ENABLE KEYS */;
 
 
 
