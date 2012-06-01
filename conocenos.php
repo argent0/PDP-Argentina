c2adcbfd23e14f1815faa9d25d9fdad12d7de3d1
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2012-06-01 04:12:00 -0300

    .

--- conocenos.php
+++ conocenos.php
@@ -15,13 +15,8 @@ if ($modo == "prod") {
 
 include("conec.php");
 
-
 /* Llamo a la funcion para que se conecte a la base, me devuelve la conexion */
 $link=Conectarse($db_host,$db_user,$db_password,$db_schema);
-
-
-$url_votamostodos = $_REQUEST["url_votamostodos"];
-$url_esta_ley = $site_url . $contexto . "ley/" . $url_votamostodos; 
 
 require 'jsonwrapper/jsonwrapper.php';
 require 'facebook-php-sdk/facebook.php';
@@ -33,53 +28,6 @@ $facebook = new Facebook(array(
 $id_facebook = $facebook->getUser();
 
 
-$leyes=mysql_query("select * from ley where url_votamostodos='$url_votamostodos'", $link);
-$ley=mysql_fetch_array($leyes);
-mysql_free_result($leyes);
-
-$accion = $_REQUEST["accion"]; 
-if ($accion=="votar_si" || $accion=="votar_no" ) {
-	//$id_facebook = $_REQUEST["id_facebook"];
-	$usuarios=mysql_query("select * from usuario where id_facebook=$id_facebook", $link);
-	$usuario=mysql_fetch_array($usuarios);
-	mysql_free_result($usuarios);
-	if (!$usuario){
-		mysql_query("insert into usuario (id_facebook) values ($id_facebook)", $link);		
-		$usuarios=mysql_query("select * from usuario where id_facebook=$id_facebook", $link);
-		$usuario=mysql_fetch_array($usuarios);
-		mysql_free_result($usuarios);
-	}		
-	$id_usuario=$usuario["id"];
-	
-	if ($accion=="votar_si") {
-		mysql_query("insert into voto (id_ley, id_usuario, voto, momento) values (" . $ley["id"] . "," . $id_usuario . ",true,now())", $link);
-		mysql_query("update ley set cant_votos_si=cant_votos_si+1 where id=". $ley["id"], $link);	
-	}
-	if ($accion=="votar_no") {
-		mysql_query("insert into voto (id_ley, id_usuario, voto, momento) values (" . $ley["id"] . "," . $id_usuario . ",false,now())", $link);
-		mysql_query("update ley set cant_votos_no=cant_votos_no+1 where id=". $ley["id"], $link);	
-	}
-	
-	$leyes=mysql_query("select * from ley where url_votamostodos='$url_votamostodos'", $link);
-	$ley=mysql_fetch_array($leyes);
-	mysql_free_result($leyes);	
-}
-
-$cant_votos = $ley["cant_votos_si"] + $ley["cant_votos_no"];
-if ($cant_votos>0) {
-	$porcentaje_si = ceil(100 * $ley["cant_votos_si"] / $cant_votos);
-	$porcentaje_no = floor(100 * $ley["cant_votos_no"] / $cant_votos);
-} else {
-	$porcentaje_si = 0;
-	$porcentaje_no = 0;
-}
-
-if ($id_facebook){
-	$votos=mysql_query("select voto.voto from voto, usuario where id_ley=" . $ley["id"] . " and voto.id_usuario=usuario.id and usuario.id_facebook=$id_facebook", $link);
-	$voto=mysql_fetch_array($votos);
-	mysql_free_result($votos);
-}
-
 function recortar($texto, $cant_caracteres){
 	if (strlen($texto) <= $cant_caracteres){
 		return $texto;
@@ -87,7 +35,10 @@ function recortar($texto, $cant_caracteres){
 		return substr($texto,0,$cant_caracteres-4) . "...";
 	}
 }
+
 ?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -97,10 +48,10 @@ function recortar($texto, $cant_caracteres){
 		<meta HTTP-EQUIV="EXPIRES" CONTENT="-1">
 		<link rel="stylesheet" type="text/css" href="<?=$contexto?>css/style.css?version=<?=filemtime('css/style.css')?>" />
 		<!-- <script type="text/javascript" src="<?=$contexto?>js/jquery-1.7.2.min.js?version=<?=filemtime('js/jquery-1.7.2.min.js')?>"></script>  -->		
-		<title>Votamos Todos - <?=$ley["titulo_lleca"]?></title>
-		<meta property="og:title" content="Ley de <?=$ley["titulo_lleca"]?>" />
+		<title>Votamos Todos - Conocenos</title>
+		<meta property="og:title" content="Votamos Todos - Conocenos" />
 		<meta property="og:type" content="cause" />
-		<meta property="og:url" content="<?=$url_esta_ley?>" />
+		<meta property="og:url" content="<?=$site_url . $contexto?>conocenos.php" />
 		<meta property="og:image" content="<?=$site_url . $contexto?>img/logo-cuadrado.jpg" />
 		<meta property="og:site_name" content="Votamos Todos" />
 		<meta property="fb:app_id" content="<?=$facebook_app_id?>" />
@@ -156,15 +107,17 @@ function recortar($texto, $cant_caracteres){
 	      });
 	    } 
 	  </script>
+	
+	  </script>
 		<div id="wrap">
 			<div class="sombra-top-wrap">
 			</div>
 			<div id="header">
 				<h2>Votamos Todos democracia participativa online</h2>
 				<div id="menu">
-					<a href="<?=$site_url . $contexto?>conocenos.php">
+					<a href="<?=$site_url . $contexto?>">
 						<div class="boton-conocenos">
-							conocenos
+							votar ahora
 						</div>
 					</a>
 					<a href="#" onclick="FB.login();return false;" id="auth-loggedout">
@@ -196,127 +149,26 @@ function recortar($texto, $cant_caracteres){
 				<!--                **********  ******************************************  **********   -->
 				<img src="img/divisor.jpg" width="984" height="3" />
 			</div>
-
-			<div id="contenido">
-				<div class="titulo-con-fondo">
-					<h2>Expediente <?=$ley["expediente"]?> : <?=recortar($ley["titulo_real"],70)?></h2>
+      
+      <div id="contenido">
+	     	<div class="titulo-con-fondo">
+       		<h2>CONOCENOS</h2>
 				</div>
-				<div class="banner-ley">
-					<div class="contenido-banner">
-						<div class="banner-parte-superior">
-							<h5><a target="newTab" href="<?=$ley["url_diputados"]?>">Leer el proyecto de ley</a></h5>
-							<div class="redes-sociales-banner">
-								<div class="fb-like" data-layout="button_count" data-width="150" data-show-faces="false" data-font="arial">
-								</div>
-								<div class="twitterBanner">
-									<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?=$url_esta_ley?>" data-text="Ley de <?=$ley["titulo_lleca"]?>" data-via="votamostodos" data-lang="es" data-dnt="true"  data-count="none">Twittear</a>
-									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> 
-								</div>
-							</div>
-						</div>
-						<h1><?=$ley["titulo_lleca"]?></h1>
-						<div class="cantidad-votos">
-							<?=$cant_votos?> votos
-						</div>
-
-						<form name="votarForm" action="<?=$url_esta_ley?>" method="POST">
-							<input id="accion" type="hidden" name="accion"/>
-						</form>
-						<script>
-							function votar(voto){
-								FB.getLoginStatus(function(response) {
-									  if (response.status === 'connected') {
-											document.getElementById('accion').value = voto;
-										  document.forms["votarForm"].submit();				    
-									  } else {
-										  FB.login();					  
-									  }
-									 });			
-							}
-						</script>
-						
-						<?
-						if ($voto){
-							if ($voto["voto"]){
-						?>		
-								<div class="banner-botones-voto-a-favor">
-									<a href="#">
-										<div class="boton-positivo" onclick="return false;">
-											<p><?=$porcentaje_si?>%</p>
-										</div>
-									</a>
-									<a href="#">
-										<div class="boton-negativo" onclick="return false;">
-											<p><?=$porcentaje_no?>%</p>
-										</div>
-									</a>
-								</div>								
-						<?
-							} else {
-						?>		
-								<div class="banner-botones-voto-en-contra">
-									<a href="#" onclick="return false;">
-										<div class="boton-positivo">
-											<p><?=$porcentaje_si?>%</p>
-										</div>
-									</a>
-									<a href="#" onclick="return false;">
-										<div class="boton-negativo">
-											<p><?=$porcentaje_no?>%</p>
-										</div>
-									</a>
-								</div>								
-						<?
-							}
-						} else {
-						?>					
-							<div class="banner-botones">
-								<a href="#" onclick="votar('votar_si');return false;">
-									<div class="boton-positivo">
-										<p><?=$porcentaje_si?>%</p>
-									</div>
-								</a>
-								<a href="#" onclick="votar('votar_no');return false;">
-									<div class="boton-negativo">
-										<p><?=$porcentaje_no?>%</p>
-									</div>
-								</a>
-							</div>								
-						<?
-						}
-						?>										
-					</div>
-				</div>
-
-				<div class="fb-comments" data-href="<?=$url_esta_ley?>" data-num-posts="8" data-width="600">
-				</div>
-				<div class="fondo-otras-leyes">
-					<div class="otras-leyes">
-						<h4>OTRAS LEYES ACTIVAS</h4>
-						<?		
-						$otras_leyes=mysql_query("select * from ley where url_votamostodos<>'$url_votamostodos' order by prioridad", $link);						
-						while($otra_ley=mysql_fetch_array($otras_leyes)) {
-						?>					
-							<h3>
-								<a href="<?=$contexto?>ley/<?=$otra_ley["url_votamostodos"]?>"><?=recortar($otra_ley["titulo_lleca"],30)?></a>
-							</h3>
-						<?		
-						}
-						mysql_free_result($otras_leyes);						
-						?>						
-					</div>
-				</div>
-
-				<!--    ESTE DIV ES HAY QUE HABILITARLO CUANDO SE QUIERAN PONERL LINKS        -->
-				<!--
-				<div class="divisor-titulo">
-					<h4>Mas informacion...</h4>
-				</div>
-				<div class="informacion-util">
-					<h3>Definicion de ley de muerte digna u ortotanasia</h3>
-				</div>
-				-->
-			</div>
+        <div class="tituloCheckbox">
+        
+    	   	<h2>&iquest;Cual es el objetivo de Votamos Todos?</h2>
+          <h3>Mejorar la calidad de la democracia consiguiendo que sea mas representativa.</h3>
+        </div>  
+        <div class="tituloCheckbox">
+        	<h2>&iquest;Ccomo se financia Votamos Todos?</h2>
+          <h3>Mejorar la calidad de la democracia consiguiendo que sea mas representativa.</h3>
+        </div>  
+        <div class="tituloCheckbox">
+        	<h2>&iquest;Cual es el objetivo de Votamos Todos?</h2>
+          <h3>Mejorar la calidad de la democracia consiguiendo que sea mas representativa. Mejorar la calidad de la democracia consiguiendo que sea mas representativa. Mejorar la calidad de la democracia consiguiendo que sea mas representativa. Mejorar la calidad de la democracia consiguiendo que sea mas representativa.</h3>
+        </div>                          
+			</div>    
+       
 			<div id="footer">
 				<div class="footer-todas-leyes">
 					<?		
